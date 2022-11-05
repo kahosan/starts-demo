f61a8bb416af836766543bbef16f87d8397fa47d
kahosan <15116355657l@gmail.com>
2022-11-05 23:34:08 +0800

    feat: use-vue script

---
+++ script/vue-config/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import './style/index.css';
+import 'virtual:uno.css';
+import 'uno.css';
+
+createApp(App).mount('#app');
--- script/vue-config/use-vue.ts
+++ script/vue-config/use-vue.ts
@@ -1,1 +1,36 @@
 import * as path from 'node:path';
+import * as fs from 'node:fs';
+import { exec } from 'node:child_process';
+
+const pwd = process.cwd();
+const currentDir = './script/vue-config';
+
+try {
+  // delete react files
+  fs.rmSync(path.join(pwd, 'src', 'main.tsx'));
+  fs.rmSync(path.join(pwd, 'src', 'App.tsx'));
+
+  const dir = fs.readdirSync(currentDir);
+
+  for (const file of dir) {
+    if (/use-vue\.ts$/.test(file)) { continue; }
+    switch (file) {
+      case 'App.vue':{
+        fs.cpSync(path.join(pwd, currentDir, file), path.join(pwd, 'src', file));
+        break;
+      }
+      case 'main.ts': {
+        fs.cpSync(path.join(pwd, currentDir, file), path.join(pwd, 'src', file));
+        break;
+      }
+      default: {
+        fs.cpSync(path.join(pwd, currentDir, file), path.join(pwd, file));
+      }
+    }
+  }
+
+  exec('pnpm i');
+  console.log('\nuse vue success');
+} catch (e) {
+  console.error(e);
+}
