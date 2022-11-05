28528dca43dad01056a1c4febc662a04fc002eaa
kahosan <15116355657l@gmail.com>
2022-11-05 23:10:20 +0800

    chore: .

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom/client';
+import App from './App';
 
 import './style/index.css';
 import 'virtual:uno.css';
 import 'uno.css';
-
-import App from './App';
 
 const rootElement = document.getElementById('root')!;
 ReactDom.createRoot(rootElement).render(
