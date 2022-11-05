5029a5475084d8d67743d45fdb05a3cf47e8a508
kahosan <15116355657l@gmail.com>
2022-11-06 00:41:15 +0800

    feat: add vue template

--- scripts/vue-config/src/App.vue
+++ scripts/vue-config/src/App.vue
@@ -1,8 +1,20 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 
+const count = ref(0);
 </script>
 
 <template>
+  <div class="max-w-600px mx-auto">
+    <div class="flex items-center flex-col mt-40">
+      <h1 text-4xl>Hello, World!</h1>
+      <h2 mb-5>This is a simple demo labs</h2>
+      <div flex>
+        <p text-xl mr-2>Count: {{ count }}</p>
+        <button w-auto px-2 py-1 border-none rounded-5 @click="() => count += 1">Click</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
