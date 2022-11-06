358ffeee73a3dac130a33aca02b11e2c89ed7e1c
kahosan <15116355657l@gmail.com>
2022-11-06 15:20:33 +0800

    feat: add react components test

---
+++ test/index.test.tsx
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import App from '../src/App';
+
+describe('<App />', () => {
+  const app = render(<App />);
+
+  it('should render', () => {
+    expect(app).toBeTruthy();
+  });
+
+  it('increment the counter', () => {
+    const button = app.getByRole('button');
+    fireEvent.click(button);
+
+    expect(app.queryByText('Count: 1')).not.toBeNull();
+    cleanup();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Unocss from 'unocss/vite';
 
 export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    exclude: ['./scripts/**/*']
+  },
   plugins: [Unocss(), react()],
   resolve: {
     alias: {
